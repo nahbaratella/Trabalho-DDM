075dadec3b5c0de8fa7c3c27c207a901f46fda63
nahbaratella <natybararella05@gmail.com>
2024-11-07 10:09:55 -0300

    Titulo

---
+++ SRC/Components/Título/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./style";
+
+export default function Title(){
+ return(
+ <View style={styles.BoxTitle}>
+ <Text style={styles.texTitle}>Trabalho DDM 3B1 G3</Text>
+ 
+ </View>
+ );
+}
---
+++ SRC/Components/Título/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+ BoxTitle: {
+ alignItems: "center",
+ },
+ texTitle: {
+    
+        color: '#697bf4',
+        fontWeight: "bold",
+        fontSize: 17,
+     
+
+ },
+});
+export default styles
